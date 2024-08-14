e620ea02fb83af57f2803b8503af2b52cd987389
purple-affogato <ivy.yiting.zhuang@gmail.com>
2024-08-14 00:57:34 -0400

    post method to fetch audio file from backend
    later will need to add smth like this to frontend: https://stackoverflow.com/questions/47686345/playing-sound-in-react-js

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_file
 from flask_cors import CORS
 from flask_restful import Api, Resource, reqparse
 import product_prices
@@ -13,14 +13,18 @@ parser.add_argument('text', type=str, help='text for tts to read out.')
 class ProductPrice(Resource):
     def get(self, product_name):
         if product_name == "":
-            return jsonify({'error': 'Produce name is missing'}), 404
+            return jsonify({'error': 'Produce name is missing'}), 400
         else:
             return jsonify(product_prices.get_product_price(product_name)), 200
 
 class FrogTTS(Resource):
     def post(self):
         args = parser.parse_args(strict=True)
-        tts.get_tts(args['text'])
+        if not args['text']:
+            return jsonify({'error': 'Produce name is missing'}), 400
+        file_path = tts.get_tts(args['text'])
+        return send_file(file_path, mimetype="audio/wav", as_attachment=True)
+
 
         
 api.add_resource(ProductPrice, '/products/<product_name>')
--- backend/tts.py
+++ backend/tts.py
@@ -5,7 +5,7 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device=device)
 
 def get_tts(text : str):
-    output_file = 'output.wav'
-    tts.tts_to_file(text=text, speaker_wav="speaker_me.wav", language="en", file_path=output_file)
+    output_file = "audio/output.wav"
+    tts.tts_to_file(text=text, speaker_wav="audio/speaker_me.wav", language="en", file_path=output_file)
     return output_file
     
