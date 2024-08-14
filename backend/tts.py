1733162701b892f8afa775bd91a76d73a0c19c68
purple-affogato <ivy.yiting.zhuang@gmail.com>
2024-08-13 20:45:42 -0400

    added xtts api

---
+++ backend/tts.py
@@ -0,0 +1,11 @@
+from TTS.api import TTS
+import torch
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device=device)
+
+def get_tts(text : str):
+    output_file = 'output.wav'
+    tts.tts_to_file(text=text, speaker_wav="speaker_me.wav", language="en", file_path=output_file)
+    return output_file
+    
