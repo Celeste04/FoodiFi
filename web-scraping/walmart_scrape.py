38c9327970be584ecda8d4e54c2897887032ab0b
purple-affogato <ivy.yiting.zhuang@gmail.com>
2024-09-03 01:34:53 -0400

    scraped multiple different products from walmart.ca

--- web-scraping/walmart_scrape.py
+++ web-scraping/walmart_scrape.py
@@ -14,17 +14,22 @@ csv_file = str(input())
 products = pd.DataFrame(columns=['Product Name', 'Product Price'])
 
 for i in range(1, pgs+1):
-    hdrs = {'User-Agent': UserAgent().random,
+    hdrs = {'User-Agent': UserAgent().chrome,
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
       'Accept-Encoding': 'none',
       'Accept-Language': 'en-US,en;q=0.8',
       'Connection': 'keep-alive'}
-    url = base_url + "?page="+str(i)
-    page = requests.get(url=url, headers=hdrs)
-    soup = BeautifulSoup(page.content, 'lxml')
-    prices = soup.find_all('div', {'data-automation-id' : 'product-price'})
-    names = soup.find_all('span', {'data-automation-id' : 'product-title'})
+    proxy_addresses = {
+    'socks5' : 'http://72.214.108.67:4145',
+    'socks5' : 'http://98.181.137.80:4145'
+    }
+    url = base_url + "&page="+str(i)
+    page = requests.get(url=url, headers=hdrs, proxies=proxy_addresses)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    # print(soup)
+    prices = soup.find_all('div', {'data-automation-id' : 'product-price'}, limit=50)
+    names = soup.find_all('span', {'data-automation-id' : 'product-title'}, limit=50)
     name_text = []
     price_text = []
     
@@ -33,7 +38,9 @@ for i in range(1, pgs+1):
         price_text.append(p.div.get_text())
 
     products = pd.concat([pd.DataFrame(zip(name_text, price_text), columns=products.columns), products], ignore_index=True)
-    time.sleep(5)
+    print(len(name_text))
+    if i < pgs:
+        time.sleep(30)
 
-products.to_csv("data/"+csv_file, index=False)
+products.to_csv("scraped-data/"+csv_file, index=False)
 # print(products)
