b0a9a2779d1c26b0568e3be2f50cc76c7942608a
Jessica Chen <jesschen046@gmail.com>
2024-09-04 13:43:46 -0400

    updated giant tiger web scraping

--- web-scraping/giant_tiger_scrape.py
+++ web-scraping/giant_tiger_scrape.py
@@ -4,6 +4,11 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
+import random
+import time
+import scraping_vars 
+import requests
+import json
 
 user_agent = UserAgent().random
 
@@ -12,6 +17,7 @@ chromedriver_path = './chromedriver.exe'
 # Set up Chrome options
 chrome_options = Options()
 chrome_options.add_argument(f'user-agent={user_agent}')
+
 
 service = Service(executable_path=chromedriver_path)
 
@@ -34,10 +40,55 @@ URL_PART_1 = "https://www.gianttiger.com/search?q="
 URL_PART_2 = "&store_inventory=bopis&online_inventory=sth"
 PAGE_URL = "&p="
 
+# in case we are differentiating by weight and package size
 AMT_RELATED_WORDS = {'pk', 'pack', 'g', 'ml'}
 
+
+def save_prev_scraped():
+    with open('prev_scraped.json', 'w') as f:
+        json.dump(scraping_vars.prev_scraped, f)
+
+def load_prev_scraped():
+    try:
+        with open('prev_scraped.json', 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+
+scraping_vars.prev_scraped = load_prev_scraped()
+
+# Generates a random proxy from the valid proxy list
+# Note: random proxy list may have to be regenerated from time to time
+def random_proxy():
+    for _ in range(30):  # Retry up to 30 times
+        proxy = random.choice(scraping_vars.valid_proxies)
+        try:
+            res = requests.get("http://ipinfo.io/json", proxies={"http": proxy, "https": proxy}, timeout=5)
+            if res.status_code == 200:
+                return proxy
+        except requests.RequestException as e:
+            print(f"Proxy {proxy} failed: {e}")
+    return None
+
+print("PREV SCRAPED")
+print(scraping_vars.prev_scraped)
+print("----------")
+
+# returns a dictionary of items and prices
 def retrieve_items_and_prices(item_name):
+    # if search has already been made before
+    if item_name in scraping_vars.prev_scraped:
+        return scraping_vars.prev_scraped[item_name]
+    
+    # initialize an item in the dictionary if not found before
+    scraping_vars.prev_scraped[item_name] = []
     while True:
+        # using a random proxy each time
+        '''
+        proxy = random_proxy()
+        if proxy:
+            chrome_options.add_argument(f'--proxy-server={proxy}')
+        '''
         page_num = 1
 
         # the url we will be searching for 
@@ -45,6 +96,7 @@ def retrieve_items_and_prices(item_name):
         # first page has a different url layout
         if (page_num == 1):
             search_url = URL_PART_1+item_name+URL_PART_2
+
         driver.get(search_url)
 
         # Let the page load completely
@@ -55,34 +107,25 @@ def retrieve_items_and_prices(item_name):
 
         # Now parse the page source with BeautifulSoup
         soup = BeautifulSoup(page_source, 'html.parser')
+        product_titles = soup.find_all('h2', class_='product-tile__title body-bold-sm')
+        product_prices = soup.find_all('span', class_='price__value')
 
-        product_titles = soup.find_all('h2', {'class':'product-tile__title body-bold-sm'})
-        product_prices = soup.find_all('span', {'class':'price__value'})
+        print("finished parsing with soup")
 
         # no more items on the page
         if (len(product_titles)) == 0:
             break
     
         # Print each product tile
-        for product_title in product_titles:
-            print(product_title)
-    
+        for i in range(len(product_titles)):
+            scraping_vars.prev_scraped[item_name].append((product_titles[i].get_text(),product_prices[i].get_text()))
         page_num+=1
+        time.sleep(5)
+    save_prev_scraped()
+    print(scraping_vars.prev_scraped)
+    return scraping_vars.prev_scraped[item_name]
 
-# tests
-search_url = "https://www.gianttiger.com/search?q=apple&store_inventory=bopis&online_inventory=sth&p=2"
-driver.get(search_url)
-
-# Let the page load completely
-driver.implicitly_wait(15)  # Wait for up to 10 seconds for elements to load
-
-# Get the page source after JS has executed
-page_source = driver.page_source
-soup = BeautifulSoup(page_source, 'html.parser')
-product_titles = soup.find_all('h2', class_='product-tile__title body-bold-sm')
-print("printing titles")
-for product_title in product_titles:
-    print(product_title)
-print("end-------")
+retrieve_items_and_prices("mayo")
+retrieve_items_and_prices("cucumber")
 driver.quit()
 
